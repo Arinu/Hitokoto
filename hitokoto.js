67fb80974a321f5572a873ce0a4233ea949f5f60
Arinu <abj5267@gmail.com>
2016-10-07 20:55:52 +0800

    update

---
+++ hitokoto.js
@@ -0,0 +1,35 @@
+window.onload=function () {
+	var gethi;
+	var body = document.querySelector('body');
+	var updatehi = document.querySelector('.sub');
+	var hitokoto = document.querySelector('.hitokoto');
+	var from = document.querySelector('.from');
+	update();
+	setcolor();
+	function update() {
+		gethi = new XMLHttpRequest();
+		gethi.open("GET","http://api.hitokoto.cn/?c=a");
+		gethi.responseType="json";
+		gethi.send();
+		gethi.onreadystatechange = function () {
+			if (true) {}
+			if (gethi.readyState===4 && gethi.status===200) {
+				hitokoto.innerHTML = gethi.response.hitokoto;
+				from.innerHTML = "- "+ gethi.response.from;
+			}
+		}
+	}
+	function setcolor() {
+		body.style.background=body.style.color=updatehi.style.background=color();
+	}
+	function color() {
+		return "rgb("+random()+","+random()+","+random()+")";
+	}
+	function random() {
+		return Math.floor(Math.random()*(101)+60)
+		}
+	updatehi.onclick=function () {
+		update();
+		setcolor();
+	}
+}
